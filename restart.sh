1eb9eb748462c6c5ea269642281dc3e314f4b552
Marco Wettstein <macrozone@marcowett2.nine.ch>
2014-08-05 00:02:08 +0200

    added

---
+++ restart.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export MONGO_URL=mongodb://localhost/scream
+export ROOT_URL=http://scream.macrozone.ch
+export PORT=8100
+forever restart $DIR/bundle/main.js
